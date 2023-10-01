ed735c03964d7e14d63efb6de45c07d7c9d5d7bc
steve0320 <srbertol@mtu.edu>
2023-09-30 20:52:32 -0700

    Stub a few random routes

--- api.py
+++ api.py
@@ -1,15 +1,32 @@
 # Main API
-from flask import Flask
+from flask import Flask, redirect
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
-    return "<p>HELLLOOOOO</p>"
+def homepage():
+    return redirect('/static/index.html')
 
 
-# Load workflow
-# Save workflow
-# Run workflow (w/ args)
-# Get nodes
+# Load a given workflow
+@app.route("/api/workflows/load")
+def load_workflow():
+    pass
+
+
+# Save a workflow (takes a built graph from the UI and serializes it to a file
+@app.route("/api/workflows/save")
+def save_workflow():
+    pass
+
+
+# Run the loaded workflow with the given args
+@app.route("/api/workflows/run")
+def run_workflow():
+    pass
+
+
+@app.route("/api/nodes")
+def get_defined_nodes():
+    pass
